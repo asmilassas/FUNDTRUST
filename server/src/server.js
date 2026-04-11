810b2a0da5176da067c2e41d35f8f7a2d71faf8c
IT23136410 <IT23136410@my.sliit.lk>
2026-04-11 16:24:49 +0530

    Minor change in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://fundtrust-laecoffas-abhishek200376s-projects.vercel.app"
+    "https://fundtrust.vercel.app/"
   ],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
